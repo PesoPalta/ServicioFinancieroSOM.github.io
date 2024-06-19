cfd9dd4411d91ba1c39a46221ee267fc2ae4d2b3
quilpalta <quilpalta@gmail.com>
2024-06-19 00:33:37 -0400

    Pequeño avance, sectorización de trabajo

--- src/app/layouts/full/sidebar/sidebar-data.ts
+++ src/app/layouts/full/sidebar/sidebar-data.ts
@@ -2,24 +2,24 @@ import { NavItem } from './nav-item/nav-item';
 
 export const navItems: NavItem[] = [
   {
-    navCap: 'Home',
+    navCap: 'Inicio',
   },
   {
-    displayName: 'Dashboard',
-    iconName: 'layout-dashboard',
-    route: '/dashboard',
+    displayName: 'Panel principal', // PRD04-RF03, PRD04-RF04 listos, falta embebir
+    iconName: 'layout-dashboard', // los PRD04-RF05 y PRD04-RF06, que pueden ser
+    route: '/dashboard', // creados desde un super usuario o simplemente normal.
+  },
+  {// Este slider pueden usarlo para crear una zona de "gestión", puede estar el 05, 06, 07, 08, 09, 10 acá.
+    navCap: 'Slider a usar', // Oscar o Matigop vean que wea ponen aca
   },
   {
-    navCap: 'Ui Components',
-  },
-  {
-    displayName: 'Badge',
-    iconName: 'rosette',
+    displayName: 'Badge', // Esto es editable, solo el displayName, queda el cambio en las
+    iconName: 'rosette', // subcategorias de abajo.
     route: '/ui-components/badge',
   },
   {
-    displayName: 'Chips',
-    iconName: 'poker-chip',
+    displayName: 'Cuentas por pagar', // Ejemplo... PRD04-RF010
+    iconName: 'poker-chip', // Si se pueden cambiar los iconos, hay que consultar bootstrap
     route: '/ui-components/chips',
   },
   {
@@ -38,18 +38,19 @@ export const navItems: NavItem[] = [
     route: '/ui-components/tooltips',
   },
   {
-    navCap: 'Auth',
+    navCap: 'Inicia sesion',
   },
   {
-    displayName: 'Login',
+    displayName: 'Inicio de sesión',
     iconName: 'lock',
     route: '/authentication/login',
   },
   {
-    displayName: 'Register',
+    displayName: 'Registro', // PRD04-RD01 Registrar usuario único
     iconName: 'user-plus',
-    route: '/authentication/register',
-  },
+    route: '/authentication/register', 
+  }, // Aqui tendría que ir otro component que sea el que permita eliminar los usuarios
+  // PRD04-RF02 Eliminar usuario.
   {
     navCap: 'Extra',
   },
